17de8a2944298fceb5c38a217b15ed980aaff71c
akshat-shethia <akshatshethia@gmail.com>
2024-01-05 18:49:18 +0530

    Linked List Questions

--- Strivers A2Z DSA Sheet/Linked List/IntroductionToLinkedList(CN)1.cpp
+++ Strivers A2Z DSA Sheet/Linked List/IntroductionToLinkedList(CN)1.cpp
@@ -10,24 +10,19 @@
  *     Node(int x, Node* next) : data(x), next(next) {}
  * };
  */
-
-// LINK -> https://www.codingninjas.com/studio/problems/introduction-to-linked-list_8144737?utm_source=youtube&utm_medium=affiliate&utm_campaign=Codestudio_Linkedlistseries&leftPanelTabValue=SUBMISSION
-
-Node *constructLL(vector<int> &arr)
-{
-    // dummy node
+ // Link -> https://www.codingninjas.com/studio/problems/introduction-to-linked-list_8144737
+Node* constructLL(vector<int>& arr) {
+    //dummy node
     Node *dummy = new Node(-1);
     Node *curr = dummy;
-    for (int i = 0; i < arr.size(); i++)
-    {
+    for(int i =0;i<arr.size();i++){
         Node *temp = new Node(arr[i]);
         curr->next = temp;
         curr = curr->next;
     }
     Node *temp = dummy->next;
-    while (temp)
-    {
-        cout << temp->data << " ";
+    while(temp){
+        cout<<temp->data<<" ";
         temp = temp->next;
     }
 }
