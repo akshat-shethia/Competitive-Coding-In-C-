d9989c63687159bcf63bce3a36949818959f5ba2
akshat-shethia <akshatshethia@gmail.com>
2024-01-19 23:25:55 +0530

    solved some misc greedy qs on codeforces

---
+++ CodeForces/Random Problems/Greedy/C_Two_Brackets.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb emplace_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp map
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+#define token(str, ch) (std::istringstream var((str)); vs v; string t; while (getline((var), t, (ch))) { v.pb(t); } return v;)
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+int main(int argc, char const *argv[])
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int n = s.size();
+        int open1 = 0;
+        int open2 = 0;
+        int ans = 0;
+
+        loop(i, 0, n - 1)
+        {
+            if (s[i] == '(')
+                open1++;
+            else if (s[i] == '[')
+                open2++;
+            else if (s[i] == ']')
+            {
+                if (open2 >= 1)
+                {
+                    open2--;
+                    ans++;
+                }
+            }
+            else
+            {
+                if (open1 >= 1)
+                {
+                    open1--;
+                    ans++;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
