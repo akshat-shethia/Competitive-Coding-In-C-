612cf2850f608b8961d90f0fd6d67ac17d47698e
akshat-shethia <akshatshethia@gmail.com>
2024-01-21 17:37:20 +0530

    just a minor change

--- Code Chef/Question Sets and Road Maps/Greedy/Maximum_Score.cpp
+++ Code Chef/Question Sets and Road Maps/Greedy/Maximum_Score.cpp
@@ -2,54 +2,54 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main()
+{
 	int t;
 	cin >> t;
-	while(t--)
+	while (t--)
 	{
-	    int n;
-	    cin >> n;
-	    long long int arr[n][n];
-	    for(int i=0;i<n;i++)
-	    {
-	        for(int j=0;j<n;j++)
-	        {
-	         cin >> arr[i][j];
-	        }
-	    }
-	    for(int i=0;i<n;i++)
-	    {
-	        sort(arr[i],arr[i]+n);
-	    }
-	    long long int sum=arr[n-1][n-1];
-	    long long int maxup=arr[n-1][n-1],flo=0;
-	    for(int i=n-2;i>=0;i--)
-	    {
-	        long long int max=0;
-	        for(int j=n-1;j>=0;j--)
-	        {
-	            if(arr[i][j]<maxup && arr[i][j]>max)
-	            {
-	                max=arr[i][j];
-	            }
-	            
-	        }
-	        if(max!=0)
-	        {
-	        maxup=max;
-	        sum+=max;
-	        }
-	        else
-	        {
-	            flo=1;
-	            break;
-	        }
-	    }
-	    if(flo==1)
-	    cout << "-1" <<"\n";
-	    else
-	    cout << sum << "\n";
-	    
+		int n;
+		cin >> n;
+		long long int arr[n][n];
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cin >> arr[i][j];
+			}
+		}
+		for (int i = 0; i < n; i++)
+		{
+			sort(arr[i], arr[i] + n);
+		}
+		long long int sum = arr[n - 1][n - 1];
+		long long int maxup = arr[n - 1][n - 1], flo = 0;
+		for (int i = n - 2; i >= 0; i--)
+		{
+			long long int max = 0;
+			for (int j = n - 1; j >= 0; j--)
+			{
+				if (arr[i][j] < maxup && arr[i][j] > max)
+				{
+					max = arr[i][j];
+				}
+			}
+			if (max != 0)
+			{
+				maxup = max;
+				sum += max;
+			}
+			else
+			{
+				flo = 1;
+				break;
+			}
+		}
+		if (flo == 1)
+			cout << "-1"
+				 << "\n";
+		else
+			cout << sum << "\n";
 	}
 	return 0;
 }
